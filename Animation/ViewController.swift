0aff0a1d52ea2478460fcacea5374e3f4d454840
Shinya-Takiyoshi <garo.savior@gmail.com>
2020-04-23 20:38:16 +0900

    基本アニメーション作成

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ImageView :UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(sender : UIButton){
+        //1.0秒かけてXが0,yが-100の動きをする
+//        UIView.animate(withDuration: 1.0){
+//            self.ImageView.transform = CGAffineTransform(translationX: 0, y: -100)
+//        }
+        // {(_)inの後に、アニメーション後の動きを書く
+                UIView.animate(withDuration: 1.0,animations: {
+                    self.ImageView.transform = CGAffineTransform(translationX: 0, y: -100)
+                }) {(_)in
+        self.ImageView.transform = CGAffineTransform.identity
+    }
 }
-
+}
